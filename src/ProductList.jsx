b07d79425bcb31e3c99c1f484b446c63645a8cad
nahobbs5 <nahobbs5@gmail.com>
2024-10-15 20:26:02 -0400

    added missing images and improved cart counter UI

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -78,13 +78,13 @@ function ProductList(props) {
             plants: [
                 {
                     name: "Lavender",
-                    image: "/lavender.jpg",
+                    image: "https://nathanhobbsportfolio.com/wp-content/uploads/2024/10/lavender.jpg",
                     description: "Calming scent, used in aromatherapy.",
                     cost: "$20"
                 },
                 {
                     name: "Jasmine",
-                    image: "/jasmine.jpg",
+                    image: "https://nathanhobbsportfolio.com/wp-content/uploads/2024/10/jasmine.jpg",
                     description: "Sweet fragrance, promotes relaxation.",
                     cost: "$18"
                 },
@@ -143,7 +143,7 @@ function ProductList(props) {
                 },
                 {
                     name: "Lavender",
-                    image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
+                    image: "https://nathanhobbsportfolio.com/wp-content/uploads/2024/10/jasmine.jpg",
                     description: "Calming scent, used in aromatherapy.",
                     cost: "$20"
                 },
@@ -201,7 +201,7 @@ function ProductList(props) {
             plants: [
                 {
                     name: "ZZ Plant",
-                    image: "https://images.unsplash.com/photo-1632207691143-643e2a9a9361?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    image: "https://nathanhobbsportfolio.com/wp-content/uploads/2024/10/zamioculcas_zamiifolia_1.jpg",
                     description: "Thrives in low light and requires minimal watering.",
                     cost: "$25"
                 },
@@ -291,9 +291,9 @@ function ProductList(props) {
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
                     
                     <div><a href="/shoppingreact/aboutUs.jsx" style={styleA}>About Us</a></div>
-                    <div><a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'>{cart.length}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div><a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><p className='cart-items'>{cart.length}</p><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div></div>
                 </div>
-            </div>
+            
             {!showCart ? (
                 <div className="product-grid">
                     {plantsArray.map((category, index) => (
